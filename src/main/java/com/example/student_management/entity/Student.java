cc1c28f7dbc33b22c5d6c62509f663d32735934c
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2025-11-06 21:26:48 +0100

    first commit

---
+++ src/main/java/com/example/student_management/entity/Student.java
@@ -0,0 +1,50 @@
+package com.example.student_management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+}
---
+++ src/main/java/com/example/student_management/repository/StudentRepository.java
@@ -0,0 +1,16 @@
+package com.example.student_management.repository;
+
+import com.example.student_management.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    // Recherche d'un étudiant par son identifiant
+    Student findById(int id);
+
+    // Requête personnalisée pour compter les étudiants par année de naissance
+    @Query("SELECT YEAR(s.dateNaissance), COUNT(s) FROM Student s GROUP BY YEAR(s.dateNaissance)")
+    Collection<Object[]> findNbrStudentByYear();
+}
